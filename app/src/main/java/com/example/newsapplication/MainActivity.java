c44e5891ab92c14702b15c06625cb3230cdd2edc
Pranavraj Goje <86144306+pranavraj-goje@users.noreply.github.com>
2022-05-16 21:52:33 +0530

    Completed till 49:10

---
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.example.newsapplication.Models.NewsApiResponse;
+import com.example.newsapplication.Models.NewsHeadlines;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    CustomAdapter adapter;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Fetching news articles..");
+        dialog.show();
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, "general", null);
+    }
+
+    private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
+        @Override
+        public void onFetchData(List<NewsHeadlines> list, String message) {
+            showNews(list);
+            dialog.dismiss();
+        }
+
+        @Override
+        public void onError(String message) {
+
+        }
+    };
+
+    private void showNews(List<NewsHeadlines> list) {
+        recyclerView = findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        adapter = new CustomAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+    }
+}
